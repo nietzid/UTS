0b48fd18c298e014b1a5282bc1d4d1ee8f0cb89e
Arfiansyah Sucitra <arfiansyah@upi.edu>
2021-04-24 00:41:13 +0700

    gabungan reza ucup

---
+++ admin/berita/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Data</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="../../css/sidebar.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Halaman Admin</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>List data</p>
+                <li class="active">
+                    <a href="../mhs/index.php">Data Mahasiswa</a>
+                </li>
+                <li>
+                    <a href="index.php">Data Berita</a>
+                </li>
+                <li>
+                    <a href="../prestasi/index.php">Data Prestasi</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+            <?php
+
+            include "koneksi.php";         
+
+            //Cek apakah ada nilai dari method GET dengan nama id_anggota
+            if (isset($_GET['id'])) {
+                $id=htmlspecialchars($_GET["id"]);
+                $sql="delete from berita where id='$id'";
+                
+                $hasil=mysqli_query($kon,$sql);
+
+                //Kondisi apakah berhasil atau tidak
+                    if ($hasil) {
+                        header("Location:index.php");
+
+                    }
+                    else {
+                        echo "<div class='alert alert-danger'> Data Gagal dihapus.</div>";
+
+                    }
+                }
+            ?>
+            
+            <a href="create.php" class="btn btn-primary" role="button">Tambah Data</a>
+            
+            <br><br>
+            <table class="table table-bordered table-hover table-dark">
+                <br>
+                <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Judul</th>
+                    <th>Isi</th>
+                    <th>Foto</th>
+                    <th colspan="3">Aksi</th>
+        
+                </tr>
+                </thead>
+                <?php
+                include "koneksi.php";
+                $sql="select * from berita order by id desc";
+        
+                $hasil=mysqli_query($kon,$sql);
+                $no=0;
+                while ($data = mysqli_fetch_array($hasil)) {
+                    $no++;
+        
+                    ?>
+                    <tbody>
+                    <tr>
+                        <td><?php echo $no;?></td>
+                        <td><?php echo $data["judul"];   ?></td>
+                        <td><?php echo $data["isi"];   ?></td>
+                        <td><img src = "foto_berita/<?= $data["foto"]; ?>" width="150"></td>
+                        <td>
+                            <a href="detail.php?id=<?php echo htmlspecialchars($data['id']); ?>" class="btn btn-info" role="button">Detail</a>
+                            <br><br>
+                            <a href="update.php?id=<?php echo htmlspecialchars($data['id']); ?>" class="btn btn-warning" role="button">Update</a> 
+                            <br><br>
+                            <a href="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?id=<?php echo $data['id']; ?>" class="btn btn-danger" role="button">Delete</a>
+                        </td>
+                    </tr>
+                    </tbody>
+                    <?php
+                }
+                ?>
+            </table>
+            
+        </div>
+    </div>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <!-- jQuery Custom Scroller CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
+</body>
+
+</html>
---
+++ admin/berita/update.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Update</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="../../css/sidebar.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Halaman Admin</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>List data</p>
+                <li class="active">
+                    <a href="../mhs/index.php">Data Mahasiswa</a>
+                </li>
+                <li>
+                    <a href="index.php">Data Berita</a>
+                </li>
+                <li>
+                    <a href="../prestasi/index.php">Data Prestasi</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+        <?php
+
+        //Include file koneksi, untuk koneksikan ke database
+        include "koneksi.php";
+
+        //Fungsi untuk mencegah inputan karakter yang tidak sesuai
+        function input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            
+            return $data;
+        }
+        //Cek apakah ada nilai yang dikirim menggunakan methos GET dengan nama id_anggota
+        if (isset($_GET['id'])) {
+            $id=input($_GET["id"]);
+            $sql="select * from berita where id=$id";
+            $hasil=mysqli_query($kon,$sql);
+            $data = mysqli_fetch_assoc($hasil);
+        }
+
+        //Cek apakah ada kiriman form dari method post
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            $id=htmlspecialchars($_POST["id"]);
+            $judul=input($_POST["judul"]);
+            $isi=input($_POST["isi"]);
+            $foto=input($_POST["foto"]);
+            $foto = upload();
+            if(!$foto){
+                return false; //apabila gagal upload maka berhenti
+            }
+            //Query update data pada tabel anggota
+            $sql="UPDATE berita set
+                judul='$judul',
+                isi='$isi',
+                foto='$foto'
+                WHERE id=$id";
+
+            //Mengeksekusi atau menjalankan query diatas
+            $hasil=mysqli_query($kon,$sql);
+
+            //Kondisi apakah berhasil atau tidak dalam mengeksekusi query diatas
+            if ($hasil) {
+                header("Location:index.php");
+            }
+            else {
+                echo "<div class='alert alert-danger'> Data Gagal diupdate.</div>";
+
+            }
+
+            
+            
+
+        }
+
+        function upload(){
+            $namaFile = $_FILES['foto']['name'];
+            $ukuranFile = $_FILES['foto']['size'];
+            $error = $_FILES['foto']['error'];
+            $tmpName = $_FILES['foto']['tmp_name'];
+            //cek apakah tidak ada foto/file yang diupload
+            if($error === 4){
+                echo "<script> alert('harap upload foto terlebih dahulu')</script>";
+                return false;
+            }
+            //membatasi jenis file yang diupload
+            $ekstenFileValid = ['jpg','jpeg','png'];
+            $ekstenFile = explode('.',$namaFile);
+            $ekstenFile = strtolower(end($ekstenFile));
+    
+            if(!in_array($ekstenFile,$ekstenFileValid)){
+                echo "<script> alert('Harap masukan file berupa jpg,jpeg, dan png!')</script>";
+                return false;
+            }
+            //set batas maks size gambar
+            if( $ukuranFile > 5000000){
+                echo "<script> alert('Ukuran gambar terlalu besar!')</script>";
+                return false;
+            }
+
+            //generate nama baru, agar tidak ada file memiliki nama yang sama dan tidak saling menimpa 
+            $namaFileBaru = uniqid();
+            $namaFileBaru .= '.';
+            $namaFileBaru .= $ekstenFile;
+        
+            
+            move_uploaded_file($tmpName,'foto_berita/'. $namaFileBaru);
+            return $namaFileBaru;
+        }
+
+        ?>
+        <h2>Update Data</h2>
+
+
+        <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" enctype="multipart/form-data">
+                <div class="form-group col-form-label-lg">
+                    <label for="">Judul</label>
+                    <input type="text" name="judul" class="form-control" value="<?php echo $data['judul']; ?>"/>
+                </div>
+
+                <div class="form-group col-form-label-lg">
+                    <label for="">Alamat :</label>
+                    <textarea name="isi" class="form-control" rows="5" placeholder="Masukan Alamat" required><?=$data['isi'];?></textarea>
+                </div>
+        
+                <div class="form-group col-form-label-lg">
+                    <label for="">Upload Foto</label>
+                    <p><img src = "foto_berita/<?= $data["foto"]; ?>" width="150"></p>
+                    <input type="file" class="form-control-file" name="foto">
+                </div>
+
+                <input type="hidden" name="id" value="<?php echo $data['id']; ?>" />
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                <a href="index.php" class="btn btn-primary" role="button">Back</a>
+            </form>
+            <br><br>
+    </div>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <!-- jQuery Custom Scroller CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+</body>
+</html>
